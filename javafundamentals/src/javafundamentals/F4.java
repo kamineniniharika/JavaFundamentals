4bb4744e10062ad4d0a8314b18579726a821fa5b
COMPUTER <COMPUTER@DESKTOP-ED65QCH>
2019-06-25 10:09:40 +0530

    first commit:num is positive or not

---
+++ javafundamentals/src/javafundamentals/F4.java
@@ -0,0 +1,13 @@
+package javafundamentals;
+
+public class F4 {
+public static void main(String args[]) {
+	int a=Integer.parseInt(args[0]);
+	if(a>0)
+		System.out.println("positive");
+	else if(a<0)
+		System.out.println("Negtive");
+	else 
+		System.out.println("Zero");
+}
+}
